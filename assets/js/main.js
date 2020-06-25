7e9082ba7b533c3b9f14477ddb38b8ae461fe044
Allythy <allythy@protonmail.com>
2020-06-25 12:37:32 -0300

    Change the form of working with us

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,3 +109,12 @@ window.onload = function () {
     },
   });
 };
+
+// form work 
+const fileInput = document.querySelector('#file-js-example input[type=file]');
+fileInput.onchange = () => {
+  if (fileInput.files.length > 0) {
+    const fileName = document.querySelector('#file-js-example .file-name');
+    fileName.textContent = fileInput.files[0].name;
+  }
+}
