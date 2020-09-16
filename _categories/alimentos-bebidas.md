79d32a9c5ce611cbc5e94403cf1dbb295aca9f0b
Allythy <allythy@protonmail.com>
2020-09-16 17:38:48 -0300

    Add prodcut imanges

--- _categories/alimentos-bebidas.md
+++
@@ -1,4 +0,0 @@
----
-name: Alimentos e bebidas
-folder: alimentos-bebidas
----
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,3 @@
-// Products
-function setcategory(category) {
-  var container = document.getElementById("category");
-  var img = container.getElementsByClassName("column");
-  for (var i = 0; i < img.length; i++) {
-    img[i].style.display = "none";
-  }
-  var show = container.getElementsByClassName(category);
-  for (var i = 0; i < show.length; i++) {
-    show[i].style.display = "flex";
-  }
-  var category_header = document.getElementsByClassName("category-header");
-  for (var i = 0; i < category_header.length; i++) {
-    var tag_category = category_header[i].getElementsByTagName("h3")[0];
-    tag_category.classList.remove("category-active");
-  }
-  var active_category = document.getElementById(category);
-  var tag_category = active_category.getElementsByTagName("h3")[0];
-  tag_category.classList.add("category-active");
-}
-document.addEventListener("DOMContentLoaded", function () {
-  setcategory("alimentos-bebidas");
-});
-
 // Menu
 document.addEventListener("DOMContentLoaded", () => {
   let navbarBurger = document.querySelector(".navbar-burger");
